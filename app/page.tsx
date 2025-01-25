816b839ab801986c099e0816d8e876b4d8ba4a2e
Wallet-bussines <pasarelawld@gmail.com>
2025-01-24 22:57:26 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import { PayBlock } from "@/components/Pay";
 
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
-      
       <PayBlock />
     </main>
   );
